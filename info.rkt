acb494bacab92ea21d7e3aced54a4e424851491a
plane <crystal@panix.com>
2021-12-31 11:57:44 -0800

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -4,7 +4,7 @@
 (define pkg-authors '("crystal@panix.com"))
 (define version "0.1")
 #;(define scribblings '(("begin-with-local.scrbl" () (library) "begin-with-local")))
-(define pkg-desc "(let [x 0 y 1] body ...) macro")
+(define pkg-desc "Syntactic sugar for splicing-let")
 (define deps '("base"))
 (define build-deps '("racket-doc"
                      "scribble-lib"))
