768811386c71b931013a6c11be9d1f7dfd22f126
Plane <crystal@panix.com>
2021-12-31 07:58:36 -0800

    Make things work even with racket/local included

--- private/syntax-classes.rkt
+++ private/syntax-classes.rkt
@@ -3,17 +3,19 @@
 (provide local-function-definition
          local-definition)
 
-(require syntax/parse)
+(require (for-syntax racket/base)
+         syntax/parse)
 
 (define-syntax-class local-function-definition
   #:description "Local function definition"
-  (pattern ((~literal local)
-            (name:id args:expr ...)
-            body:expr ...)))
+  #:datum-literals (local)
+  (pattern
+   (local (name:id args:expr ...)
+     body:expr ...)))
 
 (define-syntax-class local-definition
   #:description "Local definition"
-  (pattern ((~literal local)
-            name:id
-            value:expr)))
+  #:datum-literals (local)  
+  (pattern
+   (local name:id value:expr)))
 
