248e5074b7fd6fc10d2bb83d00b6b7f99f9eb9c5
Plane <crystal@panix.com>
2021-12-31 11:10:41 -0800

    Combine into a single syntax class

--- private/syntax-classes.rkt
+++ private/syntax-classes.rkt
@@ -1,21 +1,20 @@
 #lang racket/base
 
-(provide local-function-definition
-         local-definition)
+(provide local-definition)
 
 (require (for-syntax racket/base)
+         (for-template racket/base)
          syntax/parse)
-
-(define-syntax-class local-function-definition
-  #:description "Local function definition"
-  #:datum-literals (local)
-  (pattern
-   (local (name:id args:expr ...)
-     body:expr ...)))
 
 (define-syntax-class local-definition
   #:description "Local definition"
-  #:datum-literals (local)  
-  (pattern
-   (local name:id value:expr)))
+  #:datum-literals (local)
 
+  (pattern (local name:id value:expr))
+
+  (pattern (local (name:id args:expr ...)
+             body:expr ...)
+
+           #:attr value
+             #'(λ (args ...)
+                 body ...)))
